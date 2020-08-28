109b7a5316bd6a1513c6b209803791fd97b9b975
Yaronberg <yaronmen123@gmail.com>
2020-08-28 23:37:28 +0300

    connect swagger

---
+++ server/WebApi/ApiRoutes.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPI.Models
+{
+  public class ApiRoutes
+  {
+    public const string Api = "Api";
+    public const string Controller = "[controller]";
+    public const string Base = Api + "/" + Controller;
+  }
+}
--- server/WebApi/Startup.cs
+++ server/WebApi/Startup.cs
@@ -10,42 +10,60 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
+using WebAPI.Options;
 
 namespace WebApi
 {
-    public class Startup
+  public class Startup
+  {
+    public Startup(IConfiguration configuration)
     {
-        public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
-
-        public IConfiguration Configuration { get; }
-
-        // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
-        {
-            services.AddControllers();
-        }
-
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-        {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-
-            app.UseHttpsRedirection();
-
-            app.UseRouting();
-
-            app.UseAuthorization();
-
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapControllers();
-            });
-        }
+      Configuration = configuration;
     }
+
+    public IConfiguration Configuration { get; }
+
+    public void ConfigureServices(IServiceCollection services)
+    {
+      services.AddControllers();
+      services.AddSwaggerGen(s =>
+        {
+          s.SwaggerDoc("v1",
+            new OpenApiInfo { Title = "ToDoList API", Version = "v1" });
+        });
+    }
+
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+      if (env.IsDevelopment())
+      {
+        app.UseDeveloperExceptionPage();
+      }
+
+      var swaggerOptions = new SwaggerOptions();
+      Configuration.GetSection(nameof(SwaggerOptions)).Bind(swaggerOptions);
+
+      app.UseSwagger(option =>
+      {
+        option.RouteTemplate = swaggerOptions.JsonRoute;
+      });
+
+      app.UseSwaggerUI(option =>
+      {
+        option.SwaggerEndpoint(swaggerOptions.UiEndpoint, swaggerOptions.Description);
+      });
+
+      app.UseHttpsRedirection();
+
+      app.UseRouting();
+
+      app.UseAuthorization();
+
+      app.UseEndpoints(endpoints =>
+      {
+        endpoints.MapControllers();
+      });
+    }
+  }
 }
